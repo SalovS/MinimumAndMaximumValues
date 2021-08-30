3b0dbf4e05ea948520ba8d16745d75cf3923d967
SalovS <sviatoslavsalov@yandex.ru>
2021-08-30 16:23:49 +0300

    min and max values

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Для byte минимальное занчение " + Byte.MIN_VALUE
+                + " максимальное значение "  + Byte.MAX_VALUE);
+        System.out.println("Для short минимальное занчение " + Short.MIN_VALUE
+                + " максимальное значение "  + Short.MAX_VALUE);
+        System.out.println("Для int минимальное занчение " + Integer.MIN_VALUE
+                + " максимальное значение "  + Integer.MAX_VALUE);
+        System.out.println("Для long минимальное занчение " + Long.MIN_VALUE
+                + " максимальное значение "  + Long.MAX_VALUE);
+        System.out.println("Для float минимальное занчение " + Float.MIN_VALUE
+                + " максимальное значение "  + Float.MAX_VALUE);
+        System.out.println("Для double минимальное занчение " + Double.MIN_VALUE
+                + " максимальное значение "  + Double.MAX_VALUE);
+    }
+}
